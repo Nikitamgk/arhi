a8dd270b37b4191ef08b16cf5e30b7bbd012ecdf
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-07 07:40:35 +0700

    tryung

--- myTerm/main.c
+++ myTerm/main.c
@@ -18,7 +18,10 @@ int mt_gotoXY(int col, int row)
     printf("\033[%d;%dH", row, col);
     return 0;
 }
-
+/// @brief get screen size
+/// @param rows Y
+/// @param cols X
+/// @return 0 - good -1 - bad
 int mt_getscreensize(int * rows, int * cols)
 {
     struct winsize ws;

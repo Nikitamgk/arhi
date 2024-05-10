d95706e3ebe3030f7b254d1a2a44552204581f58
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-10 11:34:36 +0700

    merge pr02

---
+++ mySimpleComputer/sc_regGet.c
@@ -0,0 +1,15 @@
+#include "../include/mySimpleComputer.h"
+int
+sc_regGet (int registr, int* value)
+{
+	if(value == NULL || registr > 2 || registr < 0)
+		return -1;
+	else
+	{
+	int temp = flags;
+	temp << 15-registr;
+	temp >> 15+registr;
+	*value = temp;
+	return 0;
+	}
+}

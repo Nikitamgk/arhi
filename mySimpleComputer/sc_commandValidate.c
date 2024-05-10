1f91ee5969adc9430a4bc5fd56c71de35087e175
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-10 11:28:02 +0700

    yep

---
+++ mySimpleComputer/sc_commandValidate.c
@@ -0,0 +1,19 @@
+#include "../include/mySimpleComputer.h"
+int
+sc_commandValidate (int command)
+{
+	int temp = command;
+	command << 1;
+	command >> 8;
+	for (int i = 0; i <= 40; i++)
+	{
+		if(command == commands[i])
+		{
+		command = temp;
+			return 1;
+		}
+	}
+	command = temp;
+		return 0;
+
+}

c1215f09a0aca666c5ee64fb759327e666b763b9
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-10 11:34:36 +0700

    merge pr02

---
+++ mySimpleComputer/sc_memoryLoad.c
@@ -0,0 +1,13 @@
+#include "../include/mySimpleComputer.h"
+int
+sc_MemoryLoad (char* filename)
+{
+	FILE *fs;
+	if ((fs == fopen(filename, "r+")) == NULL)
+		return -1;
+	else
+	{
+		fread(&memory[0], sizeof(int), 127, fs);
+		return 0;
+	}
+}

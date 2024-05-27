735757752d0436c57f4cce63adbad8b43d29d22d
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-27 22:14:08 +0700

    formting x4

--- console/printCounters.c
+++ console/printCounters.c
@@ -1,10 +1,10 @@
-#include <console.h>
+#include "console.h"
 void
 printCounters ()
 {
   int Col, Row;
-  mt_getscreensize(&Row, &Col);
-  mt_gotoXY(62, 5);
-  printf("T: %02d IC: %04X", ICount, ICount);
+  mt_getscreensize (&Row, &Col);
+  mt_gotoXY (62, 5);
+  printf ("T: %02d IC: %04X", ICount, ICount);
   return;
 }

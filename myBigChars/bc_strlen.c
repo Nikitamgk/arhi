9e0021ea45ebc7e1862823e61c739d21c082c6a9
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-27 22:06:15 +0700

    formating bc_strlen.c

--- myBigChars/bc_strlen.c
+++ myBigChars/bc_strlen.c
@@ -3,10 +3,10 @@ int
 bc_strlen (char *str)
 {
   int length = 0;
-  for(;*str++ != '\0';length++, str++)
-  {
-    if ((*str & 0xc0) == 0x80 || str == NULL)
-      return -1;
-  }
+  for (; *str++ != '\0'; length++, str++)
+    {
+      if ((*str & 0xc0) == 0x80 || str == NULL)
+        return -1;
+    }
   return length;
 }

2ec3edc3dfeea130da526a51360c605e5d870b46
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-27 22:11:33 +0700

    formating...

--- console/test.c
+++ console/test.c
@@ -1,39 +1,40 @@
 #include "console.h"
-int main()
+int
+main ()
 {
-	int rows, cols;
-	mt_getscreensize(&rows, &cols);
-	if (cols < 90 || rows < 26)
-		return -1;
-  sc_MemoryInit();
-  mt_clrscrn();
-  for(int f = 0; f < 128; f++)
-  {
-      sc_MemorySet(f,rand()%127);
-      printCell(f, 0, 0);
-  }
-  printAccumulator();
-  printCounters();
-  printFlags();
-  printDecodedCommand(memory[0]);
-  printCommand();
-  printBigCell();
-  for(int t = 0; t < 7; t++)
-  {
-  int new_adr = rand()%10;
-  printTerm(new_adr,1);
-  }
-  mt_clrscrn();
-  for(int f = 0; f < 128; f++)
-  {
-      printCell(f, 0, 0);
-  }
-  printAccumulator();
-  printCounters();
-  printFlags();
-  printDecodedCommand(memory[0]);
-  printCommand();
-  printTerm(0,0);
-  printBigCell();
+  int rows, cols;
+  mt_getscreensize (&rows, &cols);
+  if (cols < 90 || rows < 26)
+    return -1;
+  sc_MemoryInit ();
+  mt_clrscrn ();
+  for (int f = 0; f < 128; f++)
+    {
+      sc_MemorySet (f, rand () % 127);
+      printCell (f, 0, 0);
+    }
+  printAccumulator ();
+  printCounters ();
+  printFlags ();
+  printDecodedCommand (memory[0]);
+  printCommand ();
+  printBigCell ();
+  for (int t = 0; t < 7; t++)
+    {
+      int new_adr = rand () % 10;
+      printTerm (new_adr, 1);
+    }
+  mt_clrscrn ();
+  for (int f = 0; f < 128; f++)
+    {
+      printCell (f, 0, 0);
+    }
+  printAccumulator ();
+  printCounters ();
+  printFlags ();
+  printDecodedCommand (memory[0]);
+  printCommand ();
+  printTerm (0, 0);
+  printBigCell ();
   return 0;
 }

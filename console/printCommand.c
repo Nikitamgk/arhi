29b43d81cacab06f044746fc6c39762b05ae87ba
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-27 22:14:08 +0700

    formting x4

--- console/printCommand.c
+++ console/printCommand.c
@@ -1,22 +1,22 @@
-#include <console.h>
+#include "console.h"
 void
-printCommand()
+printCommand ()
 {
   int Col, Row;
   int *sign = malloc (sizeof (int));
   int *command = malloc (sizeof (int));
   int *operand = malloc (sizeof (int));
-  mt_getscreensize(&Row, &Col);
-  mt_gotoXY(82, 5);
-  if (sc_commandDecode(memory[ICount],sign,command,operand) == -1)
-  {
-    printf("!");
-    return;
-  }
+  mt_getscreensize (&Row, &Col);
+  mt_gotoXY (82, 5);
+  if (sc_commandDecode (memory[ICount], sign, command, operand) == -1)
+    {
+      printf ("!");
+      return;
+    }
   if (*sign == 0)
-    printf("+");
+    printf ("+");
   else
-    printf("-");
-  printf("%02d : %02d", *command, *operand);
+    printf ("-");
+  printf ("%02d : %02d", *command, *operand);
   return;
 }
--- console/printTerm.c
+++ console/printTerm.c
@@ -1,30 +1,30 @@
-#include <console.h>
+#include "console.h"
 void
-printTerm(int addres, int input)
+printTerm (int addres, int input)
 {
   if (addres > 128 || addres < 0)
     return;
   if (input == 1)
-  {
-    int new_value;
-    int temp = IN_OUT_addres[1];
-    int temp_two = IN_OUT_addres[2];
-    int temp_three = IN_OUT_addres[3];
-    IN_OUT_addres[1] = IN_OUT_addres[0];
-    IN_OUT_addres[2] = temp;
-    IN_OUT_addres[3] = temp_two;
-    IN_OUT_addres[4] = temp_three;
-    IN_OUT_addres[0] = addres;
-    mt_gotoXY(67,17);
-    printf("%d< ", addres);
-    scanf("%d", &new_value);
-    sc_MemorySet(addres, new_value);
-  }
-  for(int count = 0; count <= 4; count++)
-  {
-  mt_gotoXY(66,17+count);
-  printf("%d> ", IN_OUT_addres[count]);
-  printf("%04d", memory[IN_OUT_addres[count]]);
-  }
+    {
+      int new_value;
+      int temp = IN_OUT_addres[1];
+      int temp_two = IN_OUT_addres[2];
+      int temp_three = IN_OUT_addres[3];
+      IN_OUT_addres[1] = IN_OUT_addres[0];
+      IN_OUT_addres[2] = temp;
+      IN_OUT_addres[3] = temp_two;
+      IN_OUT_addres[4] = temp_three;
+      IN_OUT_addres[0] = addres;
+      mt_gotoXY (67, 17);
+      printf ("%d< ", addres);
+      scanf ("%d", &new_value);
+      sc_MemorySet (addres, new_value);
+    }
+  for (int count = 0; count <= 4; count++)
+    {
+      mt_gotoXY (66, 17 + count);
+      printf ("%d> ", IN_OUT_addres[count]);
+      printf ("%04d", memory[IN_OUT_addres[count]]);
+    }
   return;
 }

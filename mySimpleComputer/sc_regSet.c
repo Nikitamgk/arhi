6239b4a3d7f53fd1759c9337ab3f170b2913ff69
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-27 13:29:16 +0700

    formatign x3

--- mySimpleComputer/sc_regSet.c
+++ mySimpleComputer/sc_regSet.c
@@ -2,19 +2,20 @@
 int
 sc_regSet (int registr, int value)
 {
-    if(registr == IT_MASK || registr == MC_MASK || registr == SF_MASK || registr == ZD_MASK || registr == OO_MASK)
-    {   
-    if(value == 1)
+  if (registr == IT_MASK || registr == MC_MASK || registr == SF_MASK
+      || registr == ZD_MASK || registr == OO_MASK)
     {
-        flags = flags | registr;
-        return 0;
+      if (value == 1)
+        {
+          flags = flags | registr;
+          return 0;
+        }
+      if (value == 0)
+        {
+          flags = flags & (~registr);
+          return 0;
+        }
+      return -1;
     }
-    if(value == 0)
-    {
-        flags = flags & (~registr);
-        return 0;
-    }
-    return -1;
-    }
-	return -1;
+  return -1;
 }

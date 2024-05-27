1bf24878be456d1c70ba62e8a58b8ac7cf4b6e0d
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-27 13:28:13 +0700

    formating formating

--- include/myTerm.h
+++ include/myTerm.h
@@ -4,27 +4,27 @@
 
 enum Colors
 {
-    BLACK,
-    RED,
-    GREEN,
-    BLUE,
-    MAGNETA,
-    CYAN,
-    GRAY
+  BLACK,
+  RED,
+  GREEN,
+  BLUE,
+  MAGNETA,
+  CYAN,
+  GRAY
 };
 
-int mt_clrscrn();
+int mt_clrscrn ();
 
-int mt_gotoXY(int col, int row);
+int mt_gotoXY (int col, int row);
 
-int mt_getscreensize(int * rows, int * cols);
+int mt_getscreensize (int *rows, int *cols);
 
-int mt_setfgcolor(enum Colors color);
+int mt_setfgcolor (enum Colors color);
 
-int mt_setbgcolor(enum Colors color);
+int mt_setbgcolor (enum Colors color);
 
-int mt_setdefaultcolor();
+int mt_setdefaultcolor ();
 
-int mt_set_cursorvisible(int value);
+int mt_set_cursorvisible (int value);
 
-int mt_delline();
+int mt_delline ();

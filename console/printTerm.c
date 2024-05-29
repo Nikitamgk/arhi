670b09a4237f6a482500dcb52f7343db7bbfbaf9
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-29 17:52:25 +0700

    complete pr04

--- console/printTerm.c
+++ console/printTerm.c
@@ -4,6 +4,7 @@ printTerm (int addres, int input)
 {
   if (addres > 128 || addres < 0)
     return;
+  bc_box (65, 19, 75, 25, 7, 0, "IN-OUT", 1, 0);
   if (input == 1)
     {
       int new_value;
@@ -15,15 +16,15 @@ printTerm (int addres, int input)
       IN_OUT_addres[3] = temp_two;
       IN_OUT_addres[4] = temp_three;
       IN_OUT_addres[0] = addres;
-      mt_gotoXY (67, 17);
-      printf ("%d< ", addres);
-      scanf ("%d", &new_value);
+      mt_gotoXY (67, 20);
+      printf ("%03d< ", addres);
+      scanf ("%03d", &new_value);
       sc_MemorySet (addres, new_value);
     }
   for (int count = 0; count <= 4; count++)
     {
-      mt_gotoXY (66, 17 + count);
-      printf ("%d> ", IN_OUT_addres[count]);
+      mt_gotoXY (66, 20 + count);
+      printf ("%03d> ", IN_OUT_addres[count]);
       printf ("%04d", memory[IN_OUT_addres[count]]);
     }
   return;

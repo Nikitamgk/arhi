66f163e784d3aeb4062234228065db377c56aaf7
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-29 14:07:52 +0700

    it works but... what?

--- console/printCell.c
+++ console/printCell.c
@@ -1,4 +1,26 @@
 #include "console.h"
+
+int
+sc_commandDecode (int value, int *sign, int *command, int *operand)
+{
+  int temp = value;
+  if (temp >> 14 == 1)
+    return -1;
+  else
+    {
+      temp = temp >> 14;
+      *sign = temp;
+      temp = value;
+      temp = temp << 1;
+      temp = temp >> 8;
+      *command = temp;
+      temp = value;
+      temp = temp & (~(*command << 7));
+      *operand = temp;
+      return 0;
+    }
+}
+
 void
 printCell (int address)
 {

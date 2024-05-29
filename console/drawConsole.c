395313b816d68cd29b467805f305eefa624dc10f
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-30 02:04:46 +0700

    hmm why bigcell dont printed...

--- console/drawConsole.c
+++ console/drawConsole.c
@@ -13,6 +13,7 @@ drawConsole ()
   printBigCell (memory[ICount], 65, 9);
   setlocale (LC_ALL, ".UTF-8");
   bc_box (61, 7, 114, 18, 7, 0, "Редактируемая ячейка (увеличенно)", 1, 0);
+  setlocale (LC_ALL, "");
   printKeys ();
   printTerm (0, 0);
 }
--- console/printMemory.c
+++ console/printMemory.c
@@ -4,7 +4,7 @@ printMemory ()
 {
   for (int f = 0; f < 128; f++)
     {
-      printCell (f, 0, 0);
+      printCell (f, 7, 0);
     }
   bc_box (1, 1, 60, 13, 7, 0, "Оперативная память", 1, 0);
 }

3eee7a826421678448a25fb3f091ef08735a08ac
Nikitamgk <nikita.kulikovskyi@yandex.ru>
2024-05-30 03:18:26 +0700

    formating formating

--- mySimpleComputer/sc_regGet.c
+++ mySimpleComputer/sc_regGet.c
@@ -2,13 +2,14 @@
 int
 sc_regGet (int registr, int *value)
 {
-  if (value == NULL || registr != IT_MASK || registr != ZD_MASK || registr != MC_MASK || registr != OO_MASK || registr != SF_MASK)
+  if (value == NULL || registr != IT_MASK || registr != ZD_MASK
+      || registr != MC_MASK || registr != OO_MASK || registr != SF_MASK)
     {
       return -1;
     }
   else
     {
-       *value = (flags & registr) ? 1 : 0;
+      *value = (flags & registr) ? 1 : 0;
       return 0;
     }
 }
